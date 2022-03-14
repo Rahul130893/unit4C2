c9f19ecbab149b4420b788fa3c528b92ef30f3d4
Rahul130893 <93374760+Rahul130893@users.noreply.github.com>
2022-03-14 14:03:33 +0530

    chnges in index.js

--- src/index.js
+++ src/index.js
@@ -5,13 +5,26 @@ const app=express();
 
 app.use(express.json())
 
+const usercont=require("./controllers/user.cont")
+const branchcont=require("./controllers/branchDetails.cont")
+const fixedcont=require("./controllers/fixedAccount.cont")
+
+const mastercont=require("./controllers/masterAccount.cont")
+const savingscont=require("./controllers/savingsAccount.cont")
+
 const User=require("./model/user.js")
 const Branch=require("./model/branchDetails.js")
 const Fixed=require("./model/fixedAccount.js")
 const Master=require("./model/masterAccount.js")
 const Savings=require("./model/savingsAccount.js")
 
+app.use("/user", usercont )
+app.use("/branch", branchcont)
+app.use("/fixed", fixedcont)
 
+app.use("/master", mastercont)
+
+app.use("/savings", savingscont)
 
 const connect=require("./configs/db.js")
 
